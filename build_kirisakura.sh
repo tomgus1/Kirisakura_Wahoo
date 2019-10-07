20e441634d8352c2d81c1338d5431e42ae34345e
Tim <tomgus1.2@gmail.com>
2019-10-07 23:10:09 +0200

    Typo: fix typo from merge
    Needed to allow building

--- build_kirisakura.sh
+++ build_kirisakura.sh
@@ -1,31 +1,25 @@
 #!/bin/bash
-
 echo
 echo "Clean Build Directory"
-echo 
-
+echo
 make clean && make mrproper
-
 echo
 echo "Issue Build Commands"
 echo
-
 mkdir -p out
 export ARCH=arm64
 export SUBARCH=arm64
-export CLANG_PATH=~/Android_Build/Clang_Google/linux-x86/clang-r353983d/bin
+export CLANG_PATH=~/aospa/prebuilts/clang/host/linux-x86/clang-r353983c/bin
 export PATH=${CLANG_PATH}:${PATH}
 export CLANG_TRIPLE=aarch64-linux-gnu-
-export CROSS_COMPILE=~/Android_Build/GCC_Google_Arm64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-export CROSS_COMPILE_ARM32=~/Android_Build/GCC_Google_Arm32/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
-
+export CROSS_COMPILE=~/aospa/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export CROSS_COMPILE_ARM32=~/aospa/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
 echo
 echo "Set DEFCONFIG"
-echo 
+echo
 make CC=clang O=out kirisakura_wahoo_defconfig
-
 echo
 echo "Build The Good Stuff"
-echo 
-
+echo
 make CC=clang O=out -j16
+
